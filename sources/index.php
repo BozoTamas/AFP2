b611bdc3e300202b97d9dcb7df21752aa937439a
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-02-27 20:28:54 +0100

    index.php módosítása

---
+++ sources/index.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+// require core
+require_once 'application/core/config.php';
+require_once 'application/core/app.php';
+require_once 'application/core/controller.php';
+require_once 'application/core/model.php';
+
+$app = new App;
