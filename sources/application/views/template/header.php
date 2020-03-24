6df6ff0e4643fb104ab8c2568c934bec580ed1e7
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-03-24 17:51:21 +0100

    view sablon mappa és header létrehozása

---
+++ sources/application/views/template/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="<?=site_url('assets/img/logo.png')?>" type="image/icon">
+    <title><?=SITE_NAME?></title>
+
+    <!-- Bootstrap, fontawesome, CSS -->
+    <link rel="stylesheet" href="<?=URL?>/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=URL?>/assets/vendor/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="<?=URL?>/assets/css/style.css">
+</head>
+<body>
+
+<header>
+    <nav class="bg-primary py-1">
+        <div class="container d-flex justify-content-between">
+            <a href="<?=URL?>" class="btn btn-primary"><i class="fas fa-home"></i> HOME</a>
+        </div>
+    </nav>
+</header>
