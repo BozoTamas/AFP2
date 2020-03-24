aff103e752333f4f3979c2daceca9ac6419d88be
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-03-24 17:51:59 +0100

    sablon footer hozzáadása

---
+++ sources/application/views/template/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+        <div class="float-right">&copy; <?=date("Y")?></div>
+    </div>
+</footer>
+
+<!-- jQuery, Bootstrap, JavaScript -->
+<script src="<?=URL?>/assets/vendor/jquery/jquery-3.4.1.min.js"></script>
+<script src="<?=URL?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=URL?>/assets/js/script.js"></script>
+
+</body>
+</html>
