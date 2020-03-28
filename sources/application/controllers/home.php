26f0a227a89f4e7482ddf5b0f950bb6f20d21668
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-03-28 12:00:04 +0100

    home controller megírása

--- sources/application/controllers/home.php
+++ sources/application/controllers/home.php
@@ -9,7 +9,33 @@ class Home extends Controller {
         $this->model = $this->load_model('home_model');
     }
 
-    public function index() {
-        $this->load_view('home/index');
+    public function index($alias = '') {
+        
+        if (empty($alias))
+            $this->load_view('home/index');
+        
+        else {
+            $url = $this->model->get_url($alias);
+
+            if (!empty($url))
+                $this->redirect($url);
+
+            $folder = $this->model->get_folder($alias);
+
+            if (!empty($folder))
+                $this->load_view('home/folder', $folder);
+            else
+                $this->load_view('home/index');
+        }
+    }
+
+    public function save_link() {
+        $this->model->save_link();
+        $this->redirect('/');
+    }
+
+    public function save_folder() {
+        $this->model->save_folder();
+        $this->redirect('/');
     }
 }
