3dc89dfc31e155724b1bb26a6a7300f20f8aa7a3
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-04-09 18:31:07 +0200

    home controller szerkesztés
    login és regisztrációs oldalhoz tartozó funkció megírása

--- sources/application/controllers/home.php
+++ sources/application/controllers/home.php
@@ -3,10 +3,12 @@
 class Home extends Controller {
 
     public $model;
+    public $auth_model;
 
     public function __construct() {
         parent::__construct();
         $this->model = $this->load_model('home_model');
+        $this->auth_model = $this->load_model('auth_model');
     }
 
     public function index($alias = '') {
@@ -29,13 +31,39 @@ class Home extends Controller {
         }
     }
 
-    public function save_link() {
+    public function saveLink() {
         $this->model->save_link();
         $this->redirect('/');
     }
 
-    public function save_folder() {
+    public function saveFolder() {
         $this->model->save_folder();
         $this->redirect('/');
+    }
+    
+    public function main() {
+        $this->load_view('/main');
+    }
+    
+    public function login() {
+        
+        if (empty($_POST)) {
+            $this->load_view('home/login');            
+        
+        } else {
+            $this->auth_model->login();
+            $this->redirect('/main');
+        }
+    }
+    
+    public function register() {
+        
+        if (empty($_POST)) {
+            $this->load_view('home/register');            
+        
+        } else {
+            $this->auth_model->register();
+            $this->redirect('/');
+        }
     }
 }
