2401acc5b5d120bdcf727298da34db0d1f513a2c
szpatrik98 <43348587+szpatrik98@users.noreply.github.com>
2020-04-16 18:31:40 +0200

    Support oldal átemelése.

--- sources/application/views/home/support.php
+++ sources/application/views/home/support.php
@@ -1,1 +1,56 @@
-<h1>Ide írd a support oldalra kerülő dolgokat!</h1>
+<section class="banner-area relative" id="home">	
+			<div class="overlay overlay-bg"></div>
+			<div class="container">
+				<div class="row d-flex align-items-center justify-content-center">
+					<div class="about-content col-lg-12">
+						<h1 class="text-white">
+							Contacts		
+						</h1>	
+							<p class="text-white link-nav"><a href="index.html">Home</a>  <span class="lnr lnr-arrow-right"></span>  <a href="team.html">Support</a></p>
+					</div>											
+				</div>
+			</div>
+		</section>
+		<section class="team-area section-gap team-page-teams" id="team">
+		<div class="container">
+			<div class="row d-flex justify-content-center">
+				<div class="menu-content pb-70 col-lg-8">
+					<div class="title text-center">
+						<h1 class="mb-10">Developers</h1>
+					</div>
+				</div>
+			</div>						
+			<div class="row justify-content-center d-flex align-items-center">
+				<div class="col-md-3 single-team">
+				    <div class="meta-text mt-30 text-center">
+					    <h4>Bozó Tamás Dániel - J7Y3U4</h4>
+					    <p>btd2k17@gmail.com</p>									    	
+				    </div>
+				</div>
+				<div class="col-md-3 single-team">
+				    <div class="meta-text mt-30 text-center">
+					    <h4>Horváth Dániel - </h4>
+					    <p>...@gmail.com</p>			    	
+				    </div>
+				</div>
+				<div class="col-md-3 single-team">
+				    <div class="meta-text mt-30 text-center">
+					    <h4>Rieger Máté József - </h4>
+					    <p>...@gmail.com</p>			    	
+				    </div>
+				</div>
+				<div class="col-md-3 single-team">
+				    <div class="meta-text mt-30 text-center">
+					    <h4>Szabó Patrik - </h4>
+					    <p>...@gmail.com</p>			    	
+				    </div>
+				</div>
+				<div class="col-md-3 single-team">
+				    <div class="meta-text mt-30 text-center">
+					    <h4>Veréb Dávid - </h4>
+					    <p>...@gmail.com</p>			    	
+				    </div>
+				</div>
+			</div>
+		</div>	
+	</section>
