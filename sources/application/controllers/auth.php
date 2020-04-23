9e8b65b2148928684e4bbcbe8d34c0d1788ee82a
Veréb Dávid <verebdavid2@gmail.com>
2020-04-23 14:33:24 +0200

    Még kell jópár javítás

---
+++ sources/application/controllers/auth.php
@@ -0,0 +1,35 @@
+<?php
+
+class Auth extends Controller {
+	
+	public $model;
+	
+	public function __construct() {
+        parent::__construct();
+        $this->model = $this->load_model('auth_model');
+    }
+
+public function validate_Member() {
+        $this->model->validate_Member();
+        $this->redirect('/');
+    }
+
+public function user_Exists() {
+        $this->model->user_Exists();
+        $this->redirect('/');
+    }
+	
+public function new_User() {
+        $this->model->new_User();
+        $this->redirect('/');
+    }
+	
+public function user_Login() {
+		$this->model->user_Login();
+		$this->redirect('/');
+		
+public function user_ForgotPassword
+		$this->model->user_ForgotPassword();
+		$this->redirect('/');
+
+}
--- sources/application/models/auth_model.php
+++ sources/application/models/auth_model.php
@@ -1,5 +1,83 @@
 <?php
 
 class auth_model extends Model {
+	
+	public function validate_Member() {
 
+    $valid = true;
+    $errorMessage = array();
+    foreach ($_POST as $key => $value) {
+        if (empty($_POST[$key])) {
+            $valid = false;
+        }
+    }
+	
+	if($valid == true) {
+        if ($_POST['password'] != $_POST['password_confirm']) {
+            $errorMessage[] = 'A jelszónak egyeznie kell';
+            $valid = false;
+        }
+	}
+        
+        if (!isset($error_message)) {
+            if (! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                $errorMessage[] = "Érvénytelen e-mail cím!";
+                $valid = false;
+            }
+        }
+		
+	public function user_Exists($username, $email) {
+
+    $query = "SELECT * FROM `users` WHERE `name` = ? OR `email` = ?";
+    $paramType = "ss";
+    $paramArray = array($username, $email);
+    $memberCount = $this->ds->numRows($query, $paramType, $paramArray);
+    
+    return $memberCount;
+	}
+		
+	public function new_User($username, $password, $email) {
+		
+		global $errors; //hibák számát tárolja, ha nagyobb mint 0, nem engedi a reget
+
+    $passwordHash = md5($password);
+    $query = "INSERT INTO `users` (`name`, `email`, `password`) VALUES (?, ?, ?)";
+    $paramType = "ssss";
+    $paramArray = array(
+        $username,
+		$email
+        $passwordHash,
+    );
+	/*if (empty($username)) {  //vizsgálja, hogy beírt-e valamit a júzer az adott helyre
+		array_push($errors, "Írjon be felhasználót");  
+	}
+	if (empty($email)) { 
+		array_push($errors, "Írjon be e-mail címet"); 
+	}
+	if (empty($password_1)) { 
+		array_push($errors, "Írjon be jelszót"); 
+	}
+	*///if (count($errors) == 0) { //ha 0 a hibaszám akkor feltölti az adatbázisba
+    $insertId = $this->ds->insert($query, $paramType, $paramArray);
+    return $insertId;
+	//	}
+	}
+	
+	public function user_Login($username, $password) {
+		global $errors;
+	if (empty($username)) {
+		array_push($errors, "A bejelentkezéshez szükséges a felhasználónév");
+	}
+	if (empty($password)) {
+		array_push($errors, "A bejelentkezéshez szükséges a jelszó");
+	}
+	if (count($errors) == 0 }
+	$query = "SELECT * FROM `users` WHERE `name` = ? OR `password` = ?";
+	$result = mysqli_query($db, $query);
+		}
+	}
 }
+
+	
+
+	
