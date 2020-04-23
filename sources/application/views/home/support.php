00b05dc0e167722a8216dd3fc92760fcaecaf3e1
Bozó Tamás Dániel <btd2k17@gmail.com>
2020-04-23 10:34:51 +0200

    Update + kódszépítés

--- sources/application/views/home/support.php
+++ sources/application/views/home/support.php
@@ -1,53 +1,62 @@
 <section class="banner-area relative">	
-			<div class="overlay overlay-bg"></div>
-			<div class="container">
-				<div class="row d-flex align-items-center justify-content-center">
-					<div class="about-content col-lg-12">
-						
-					</div>											
-				</div>
-			</div>
-		</section>
-		<section class="team-area section-gap team-page-teams" id="team">
+	<div class="overlay overlay-bg">
 		<div class="container">
-			<div class="row d-flex justify-content-center">
-				<div class="menu-content pb-70 col-lg-8">
-					<div class="title text-center">
-						<h1 class="mb-10">Developers</h1>
-					</div>
-				</div>
-			</div>						
-			<div class="row justify-content-center d-flex align-items-center">
-				<div class="col-md-3 single-team">
-				    <div class="meta-text mt-30 text-center">
-					    <h4>Bozó Tamás Dániel - J7Y3U4</h4>
-					    <p>btd2k17@gmail.com</p>									    	
-				    </div>
-				</div>
-				<div class="col-md-3 single-team">
-				    <div class="meta-text mt-30 text-center">
-					    <h4>Horváth Dániel - </h4>
-					    <p>...@gmail.com</p>			    	
-				    </div>
-				</div>
-				<div class="col-md-3 single-team">
-				    <div class="meta-text mt-30 text-center">
-					    <h4>Rieger Máté József - </h4>
-					    <p>...@gmail.com</p>			    	
-				    </div>
-				</div>
-				<div class="col-md-3 single-team">
-				    <div class="meta-text mt-30 text-center">
-					    <h4>Szabó Patrik - </h4>
-					    <p>...@gmail.com</p>			    	
-				    </div>
-				</div>
-				<div class="col-md-3 single-team">
-				    <div class="meta-text mt-30 text-center">
-					    <h4>Veréb Dávid - </h4>
-					    <p>...@gmail.com</p>			    	
-				    </div>
+			<div class="row d-flex align-items-center justify-content-center">
+				<div class="about-content col-lg-12">
+				</div>											
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="team-area section-gap team-page-teams" id="team">
+	<div class="container">
+	
+		<div class="row d-flex justify-content-center">
+			<div class="menu-content pb-70 col-lg-8">
+				<div class="title text-center">
+					<h1 class="mb-10">Developers</h1>
 				</div>
 			</div>
-		</div>	
-	</section>
+		</div>		
+		
+		<div class="row justify-content-center d-flex align-items-center">
+		
+			<div class="col-md-3 single-team">
+			    <div class="meta-text mt-30 text-center">
+				    <h4>Bozó Tamás Dániel - J7Y3U4</h4>
+				    <p>btd2k17@gmail.com</p>									    	
+			    </div>
+			</div>
+			
+			<div class="col-md-3 single-team">
+			    <div class="meta-text mt-30 text-center">
+				    <h4>Horváth Dániel - </h4>
+				    <p>...@gmail.com</p>			    	
+			    </div>
+			</div>
+			
+			<div class="col-md-3 single-team">
+			    <div class="meta-text mt-30 text-center">
+				    <h4>Rieger Máté József - </h4>
+				    <p>...@gmail.com</p>			    	
+			    </div>
+			</div>
+			
+			<div class="col-md-3 single-team">
+			    <div class="meta-text mt-30 text-center">
+				    <h4>Szabó Patrik - </h4>
+				    <p>...@gmail.com</p>			    	
+			    </div>
+			</div>
+			
+			<div class="col-md-3 single-team">
+			    <div class="meta-text mt-30 text-center">
+				    <h4>Veréb Dávid - </h4>
+				    <p>...@gmail.com</p>			    	
+			    </div>
+			</div>
+			
+		</div>
+	</div>	
+</section>
