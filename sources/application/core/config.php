ed6e94b6c5bbc55d9e6db4fea574b98c266f656c
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-04-30 13:34:28 +0200

    config módosítás
    Az adatbázis átkerül localhostra, mert nagyon lassú az ami a neten van (nem találtam használhatót)

--- sources/application/core/config.php
+++ sources/application/core/config.php
@@ -10,16 +10,16 @@ define('MAIN_CONTROLLER', 'home');
 define('MAIN_VIEW', 'index');
 
 // DB Params
-define('DB_HOST', 'db4free.net');
-define('DB_NAME', 'afp2_link');
-define('DB_USER', 'afp2_link');
-define('DB_PASS', 'afp2_link');
+define('DB_HOST', 'localhost'); //'db4free.net');
+define('DB_NAME', 'afp2'); //'afp2_link');
+define('DB_USER', 'root'); //'afp2_link');
+define('DB_PASS', ''); //afp2_link');
 define('DB_PORT', 3306);
 define('DB_TYPE', 'mysql');
 define('DB_CHARSET', 'UTF-8');
 
 // Metadata
-define('SITE_NAME', 'Link to you');
+define('SITE_NAME', 'Link for you');
 define('SITE_DESCRIPTION', '');
 define('APP_VERSION', '0.1');
 define('APP_LAST_UPDATE', '');
