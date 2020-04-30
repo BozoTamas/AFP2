c9ce26c4e91c2266b8104804a63d21cfffa67187
Horváth Dániel <43348597+horvath95d@users.noreply.github.com>
2020-04-30 13:41:13 +0200

    dir.php létrehozása
    Amikor valaki feltöltötte a fájljait és elküldi a linket. pl.: localhost/asd, akkor ez az oldal tölt be és itt megjelenik a localhost/assets/uploads/asd/ mappában feltöltött fájlok listája

---
+++ sources/application/views/home/dir.php
@@ -0,0 +1,1 @@
+<h1>Amikor valaki feltöltötte a fájljait és elküldi a linket. pl.: localhost/asd, akkor ez az oldal tölt be és itt megjelenik a localhost/assets/uploads/asd/ mappában feltöltött fájlok listája</h1>
