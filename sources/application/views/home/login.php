44172b397cded085362b3940277b6e6ba7961985
riegermate97 <rieger.mate@gmail.com>
2020-05-07 15:10:57 +0200

    banner-area relative

---
+++ sources/application/views/home/login.php
@@ -0,0 +1,16 @@
+<div align = "center">
+<section class="banner-area relative">
+    <form action="saveLink" method="POST">
+        <a>User name:</a>
+        </br>
+        <input type="text" name="username">
+        </br>
+		<P align="center" style="margin-top:10;">
+        <a>Password:</a>
+        </br>
+        <input type="text" name="password">
+        </br>
+        </br>
+        <button type="submit" class="btn btn-dark">Login</button>
+    </form>
+</section>
