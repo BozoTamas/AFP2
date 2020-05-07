771a572bf6e427952d4c22cf6c8f49271e712b42
riegermate97 <rieger.mate@gmail.com>
2020-05-07 15:11:21 +0200

    banner-area relative hozzáadása

---
+++ sources/application/views/home/megjelenites.php
@@ -0,0 +1,13 @@
+<div align = "center">
+<section class="banner-area relative">
+    <form action="saveLink" method="POST">
+        <a>URL:</a>
+        </br>
+		<P align="center" style="margin-top:10;">
+        <input type="text" name="megjelenites">
+        </br>
+		<P align="center" style="margin-top:10;">
+        <button type="submit" class="btn btn-dark">Megjelenítés</button>
+    </form>
+</section>
+ 
