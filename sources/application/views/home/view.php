b79b7374acd7e8ca1ff8e4a53e823bf7b8a05637
szpatrik98 <szabbom98@gmail.com>
2020-05-07 21:13:29 +0200

    view szerkesztése

--- sources/application/views/home/view.php
+++ sources/application/views/home/view.php
@@ -1,13 +1,45 @@
-<div align = "center">
-<section class="banner-area relative">
-    <form action="saveLink" method="POST">
-        <a>URL:</a>
-        </br>
-		<P align="center" style="margin-top:10;">
-        <input type="text" name="megjelenites">
-        </br>
-		<P align="center" style="margin-top:10;">
-        <button type="submit" class="btn btn-dark">Megjelenítés</button>
-    </form>
+
+<section >
+    <div align = "left">
+     <table class="h3 text-black">
+      <tr>
+         <td > <i class="fa fa-user" aria-hidden="true"></i>id:</td>
+         <td> <?=$data['id']?></td>
+      </tr>
+      <tr>
+          <td><i class="fa fa-tag" aria-hidden="true"></i>alias:</td>
+          <td> <?=$data['alias']?></td>
+      </tr>
+      <tr>
+          <td><i class="fa fa-eye" aria-hidden="true"></i>views:</td>
+          <td> <?=$data['views']?></td>
+      </tr>
+      <tr>
+          <td ><i class="fa fa-calendar" aria-hidden="true"></i>created:</td>
+          <td> <?=$data['created']?></td>
+      </tr>
+         <tr>
+          <td><i class="fa fa-calendar" aria-hidden="true"></i>expiration:</td>
+          <td> <?=$data['expiration']?></td>
+      </tr>
+     </table>      
+     
+
+     </div>
+     <div align = "center">
+     <?php
+        
+        $dir = "./assets/uploads/asd";
+
+        // Open a directory, and read its contents
+        if (is_dir($dir)){
+          if ($dh = opendir($dir)){
+            while (($file = readdir($dh)) !== false){
+              echo "filename:" . $file . "<br>";
+            }
+            closedir($dh);
+          }
+        }
+        ?>
+    </div>
 </section>
- 
