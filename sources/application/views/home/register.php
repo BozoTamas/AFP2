d31bd100db6d6618bb1a94a4450f9bfd9a19247e
riegermate97 <rieger.mate@gmail.com>
2020-05-07 15:12:25 +0200

    banner-area relative hozzáadása

---
+++ sources/application/views/home/register.php
@@ -0,0 +1,27 @@
+<div align = "center">
+<section class="banner-area relative">
+    <form action="saveLink" method="POST">
+        <a>User name:</a>
+        </br>
+        <input type="text" name="username">
+        </br>
+		<P align="center" style="margin-top:10;">
+        <a>Password:</a>
+        </br>
+        <input type="text" name="password">
+        </br>
+		<P align="center" style="margin-top:10;">
+         <a>Password again:</a>
+        </br>
+        <input type="text" name="password">
+        </br>
+		<P align="center" style="margin-top:10;">
+         <a>E-mail addres:</a>
+        </br>
+        <input type="text" name="email">
+        </br>
+        </br>
+        <button type="submit" class="btn btn-dark">Registration</button>
+    </form>
+</section>
+  
